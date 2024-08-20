977222a0beab62280b0260da2bc64a1e4c214e3f
anemochore <fallensky@naver.com>
2024-08-20 10:02:52 +0900

    API 추가, 아마존 일본 수정 등등등git add .!

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # amazon-sample-trans
-amazon sample auto ocr &amp; trans (via google api) tampermonkey script
+amazon sample auto ocr &amp; trans (via google, etc api) tampermonkey script
 
 ## 문제의식 & 기능
 1. 이미지로 뜨는 아마존(미국 및 일본) 미리보기에도 자동 번역을 적용하고 싶다.
 2. 크롬 브라우저의 구글 렌즈를 실행해도 번역은 적용할 수가 없다.
 3. 미리보기는 `iframe`으로 뜨므로 콘솔창에서 그냥 자바스크립트를 실행해서는 이미지에 접근할 수가 없다.
 4. 그래서 탬퍼멍키의 `@match`를 `iframe` 주소로 지정해서 자동으로 실행되게 만들 수밖에 없었다.
-5. 이미지에 대해서는 Cloud Vision API로 OCR을 돌리고, 그 텍스트에 대해 Cloud Translation API로 번역을 실행하게 했다.
+5. 이미지에 대해서는 Cloud Vision API로 OCR을 돌리고, 그 텍스트에 대해 Cloud Translation API로 번역을 실행하게 했다. -> 구글이 비싸서 네이버 OCR과 딥엘 번역도 추가했다(네이버 번역은 무료가 없어서 제외).
 6. 이미지들이 레이지 로딩되므로 다 뜰 때까지 기다려야 해서 예전에 개조한 `elementReady()`를 썼다.
 
 ## 사용법
-1. 구글 클라우드 콘솔에 프로젝트를 만들고 Cloud Vision API와 Cloud Translation API를 사용 설정하고 API 키를 적어둔다.
+1. 사용할 OCR 및 번역 API를 정하자. 가령 구글을 사용할 거라면 구글 클라우드 콘솔에 프로젝트를 만들고 Cloud Vision API와 Cloud Translation API를 사용 설정하고 API 키를 적어둔다. 네이버, 딥엘도 마찬가지.
 2. 탬퍼멍키에 app.js를 추가하고 탬퍼멍키 설정을 '상급자'로 바꾼 다음 아마존 미리보기를 한 번 띄우면 탬퍼멍키 저장소에 API 키를 적으라고 나올 거다.
-3. 탬퍼멍키 저장소에 가서 `GOOGLE_API_KEY`에 자신의 API 키를 적는다.
-4. 다시 아마존 페이지에 접속해 새로고침하고 미리보기를 보면 옆에 번역이 나옴.
+3. 사용할 OCR 및 번역 API의 크리덴셜을 탬퍼멍키 저장소에 적는다.
+    1. OCR을 [구글](https://cloud.google.com/vision/docs/ocr)로 사용하려면 `"GOOGLE_OCR_API_KEY"`에 본인 API 키를 적는다. 
+    2. OCR을 [네이버](https://api.ncloud-docs.com/docs/ai-application-service-ocr)로 사용하려면 `"NAVER_OCR_SECRET"`에 클로바 시크릿을 적고 `"NAVER_OCR_URL"`에 APIGW Invoke URL을 적는다(개발자는 'API Gateway 자동 연동'을 켜고 테스트했다).
+    3. 번역을 [구글](https://cloud.google.com/translate/docs)로 사용하려면 `"GOOGLE_TRANS_API_KEY"`에 본인 API 키를 적는다(OCR과 동일한 프로젝트라면 API 키도 같다).
+    4. 번역을 [딥엘](https://developers.deepl.com/docs)로 사용하려면 `"DEEPL_API_KEY"`에 본인 API 키를 적는다(무료 사용자용으로 하드코딩).
+4. 다시 아마존 페이지에 접속해 새로고침하고 미리보기를 눌러보면 옆에 번역이 뜸.
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         amazon sample trans
 // @namespace    http://tampermonkey.net/
-// @version      0.0.8
+// @version      0.1.0
 // @updateURL    https://raw.githubusercontent.com/anemochore/amazon-sample-trans/main/app.js
 // @downloadURL  https://raw.githubusercontent.com/anemochore/amazon-sample-trans/main/app.js
 // @description  try to take over the world!
@@ -9,80 +9,255 @@
 // @match        https://read.amazon.com/sample/*
 // @match        https://read.amazon.co.jp/sample/*
 // @grant        GM_getValue
-// @grant        GM_setValue
+// @grant        GM_xmlhttpRequest
+// @connect      apigw.ntruss.com
+// @connect      googleapis.com
+// @connect      deepl.com
 // ==/UserScript==
 
-const API_KEY = GM_getValue('GOOGLE_API_KEY');
+const ocrs = {}, translates = {};
+ocrs.naver = async function(imgUrlOrB64) {
+  //네이버 ocr(월 100건(?) 무료)
+
+  const reqImage = {name: 'whatever'};
+  if(imgUrlOrB64.startsWith('http')) {
+    reqImage.format = imgUrlOrB64.split('?').shift().split('.').pop();
+    reqImage.url= imgUrlOrB64;
+  }
+  else {
+    reqImage.format = 'png',
+    reqImage.data = imgUrlOrB64;
+  };
+
+  let res, data, text;
+  try {
+    const timeStamp = Date.now();
+    
+    const clovaEndpoint = ocrUrl;
+    res = await fetchCors(clovaEndpoint, {
+    //res = await fetch(clovaEndpoint, {
+      method: 'POST',
+      headers: {
+        'X-OCR-SECRET': ocrKey,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        version: 'V2',
+        requestId: 'id_' + timeStamp,
+        timestamp: timeStamp,
+        lang: 'ja',  //english is supported by default
+        images: [reqImage],
+        //enableTableDetection: true,
+      }),
+    });
+    //data = await res.json();
+    data = res.response;
+    if(data.images[0]?.inferResult == 'SUCCESS') {
+      const imageFields = data.images[0].fields;
+      const inferTexts = imageFields.map(el => el.inferText),
+            lineBreaks = imageFields.map(el => el.lineBreak ? '\n' : ' ');
+      text = zip(inferTexts, lineBreaks).map(el => el.join('')).join('');
+    }
+    console.log('text:', text);
+  }
+  catch(e) {
+    console.log('naver ocr failed', e);
+  }
+
+  return text;
+
+  function zip(...rows) {
+    //https://stackoverflow.com/a/10284006/6153990
+    return [...rows[0]].map((_,c) => rows.map(row => row[c]));
+  }
+};
+ocrs.google = async imgUrlOrB64 => {
+  //구글 ocr(월 이미지 1000개 무료)
+
+  let reqImage;
+  if(imgUrlOrB64.startsWith('http')) reqImage = {source: {imageUri: imgUrlOrB64}};
+  else reqImage = {content: imgUrlOrB64};
+  
+  let res, data, text;
+  try {
+    res = await fetch(`https://vision.googleapis.com/v1/images:annotate?key=${ocrKey}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        requests: [{
+          features: [{type: "TEXT_DETECTION"}],
+          image: reqImage,
+        }],
+      }),
+    });
+    data = await res.json();
+    text = data.responses?.pop().fullTextAnnotation.text?.replaceAll(/Copyrighted Material/g, '').replaceAll(/\n/gm, '<br/>');
+    console.log('text:', text);
+  }
+  catch(e) {
+    console.log('google ocr failed', e);
+  }
+
+  return text;
+};
+translates.google = async (text, target = navigator.language.slice(0, 2)) => {
+  //구글 번역(월 50만 자 무료)
+
+  let res, data, translation;
+  try {
+    res = await fetch(`https://translation.googleapis.com/language/translate/v2?key=${ocrKey}&q=${encodeURIComponent(text)}&target=${target}`);
+    data = await res.json();
+    translation = data.data?.translations.pop().translatedText.replaceAll(/<br\/>/g, '\n');
+    console.log('translation:', translation);
+  }
+  catch(e) {
+    console.log('google trans failed', e);
+  }
+
+  return translation;
+};
+translates.deepl = async (text, target = navigator.language.slice(0, 2)) => {
+  //딥엘 무료 번역(월 50만 자 무료)
+
+  let res, data, translation;
+  //try {
+    res = await fetchCors(`https://api-free.deepl.com/v2/translate`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `DeepL-Auth-Key ${transKey}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        text: [text],
+        target_lang: target,
+      }),
+    });
+    data = res.response;
+    translation = data?.translations.pop().text.replaceAll(/<br\/>/g, '\n');
+    console.log('translation:', translation);
+  /*
+  }
+  catch(e) {
+    console.log('deepl trans failed', e);
+  }
+  */
+
+  return translation;
+};
+
 
 const output = document.createElement('textarea');
-output.spellcheck = false;
-output.style.top = '12%';
-output.style.left = '65.75%';
-output.style.width = '674px';
-output.style.height = '85.5%';
-output.style.position = 'absolute';
-output.style.fontSize = '2em';
-output.style.lineHeight = '1.5em';
-output.style.backgroundColor = 'lightgray';
-output.style.overflow = 'scroll';
-output.style.zIndex = '9999';
-document.body.append(output);
-output.value = '실행 중...';
+let root, SINGLE_PAGE_FLAG, texts, translations;
+let ocrKey, ocrFunc, ocrUrl, transKey, transFunc;
+await init();
+if(!ocrFunc || !transFunc) return;
 
-if(!API_KEY || API_KEY == 'enter your key here!!!') {
-  GM_setValue('GOOGLE_API_KEY', 'enter your key here!!!');
-  output.value = '탬퍼멍키 저장소에 본인 구글 API 키를 써야 합니다.';
-  return;
-}
-
-let SINGLE_PAGE_FLAG = false;
-if(location.host == 'read.amazon.co.jp') SINGLE_PAGE_FLAG = true;
-
-let observer, texts, translations;
-let imgsLength;
-
-if(!SINGLE_PAGE_FLAG) texts = [], translations = [];
-else                  texts = {}, translations = {};
+let observer, imgsLength;
 observer = new MutationObserver(onLoad);
 await onLoad();  //force first run
+
+
+async function init() {
+  //output
+  output.spellcheck = false;
+  output.style.top = '12%';
+  output.style.left = '65.75%';
+  output.style.width = '674px';
+  output.style.height = '85.5%';
+  output.style.position = 'absolute';
+  output.style.fontSize = '2em';
+  output.style.lineHeight = '1.5em';
+  output.style.backgroundColor = 'lightgray';
+  output.style.overflow = 'scroll';
+  output.style.zIndex = '9999';
+  document.body.append(output);
+  output.value = '실행 중...';
+
+  //ocr: google, naver 순서
+  ocrKey = GM_getValue('GOOGLE_OCR_API_KEY');
+  if(!ocrKey && !GM_getValue('DEEPL_API_KEY') && GM_getValue('GOOGLE_API_KEY')) ocrKey = GM_getValue('GOOGLE_API_KEY');
+
+  if(ocrKey) ocrFunc = ocrs.google;
+  else {
+    ocrKey = GM_getValue('NAVER_OCR_SECRET');
+    if(ocrKey) {
+      ocrFunc = ocrs.naver;
+      ocrUrl = GM_getValue('NAVER_OCR_URL');
+      if(!ocrUrl) {
+        output.value = '탬퍼멍키 저장소에 네이버 OCR 게이트웨이 URL(NAVER_OCR_URL)도 써야 합니다.';
+        ocrFunc = null;
+        return;
+      }
+    }
+    else {
+      output.value = '탬퍼멍키 저장소에 본인 구글 OCR API 키(GOOGLE_OCR_API_KEY)를 쓰든가 네이버 OCR 시크릿 & OCR 게이트웨이 URL(NAVER_OCR_URL)을 써야 합니다.';
+      return;
+    }
+  }
+
+  //번역: google, deepl 순서
+  transKey = GM_getValue('GOOGLE_TRANS_API_KEY') || GM_getValue('GOOGLE_API_KEY');
+  if(transKey) transFunc = translates.google;
+  else {
+    transKey = GM_getValue('DEEPL_API_KEY');
+    if(transKey) transFunc = translates.deepl;
+    else {
+      output.value = '탬퍼멍키 저장소에 본인 구글 번역 API 키(GOOGLE_TRANS_API_KEY)를 쓰든가 딥엘 API 키(DEEPL_API_KEY)를 써야 합니다.';
+      return;
+    }
+  }
+
+  //entry
+  root = await elementReady_('div#kr-renderer');
+  SINGLE_PAGE_FLAG = root.querySelector('div[data-page]') ? false : true;
+
+  if(SINGLE_PAGE_FLAG) {
+    texts = {}, translations = {};
+  }
+  else {
+    texts = [], translations = [];
+  }
+}
 
 async function onLoad() {
   observer.disconnect();
 
-  let keys = [], imgUrlOrB64s;
+  const keys = [], imgUrlOrB64s = {};
   if(!SINGLE_PAGE_FLAG) {
     await elementReady_('div[data-page]', document, {checkIfAllChildrenAreAdded: true});
     const imgs = await elementReady_('div[data-page]>img', document, {returnAll: true, checkIfAllChildrenAreAdded: true});
-    //console.log('prev imgs length, imgs length', imgsLength, imgs.length);
+    console.log('prev imgs length, imgs length', imgsLength, imgs.length);
 
     if(!imgsLength || imgs.length > imgsLength) {
       keys = imgs.map(el => el.parentElement.getAttribute('data-page'));
-      imgUrlOrB64s = [];
       keys.forEach((key, i) => {
         if(texts[key]) keys[i] = null;
-        else imgUrlOrB64s[i] = imgs[i].src;
+        else imgUrlOrB64s[key] = imgs[i].src;
       });
       imgsLength = imgs.length;
     }
   }
   else {
-    const div = await elementReady_('div.kg-full-page-img');
-    const img = await elementReady_('img', div);
+    root = await elementReady_('div#kr-renderer');  //need to refresh root when single page
+    let img = await elementReady_('img', root, {checkIfAllChildrenAreAdded: true});
+    let key = img.src.split('/').pop();
 
-    const key = img.src.split('/').pop();
-    keys = [key];
-    imgUrlOrB64s = {};
+    keys[0] = key;
     imgUrlOrB64s[key] = convertImageToBase64(img).replace('data:image/png;base64,', '');
   }
 
+  let prevOutput = output.value;
   for(const key of keys) {
-    if(!texts[key]) texts[key] = await ocr(imgUrlOrB64s[key]) || ' ';
-    if(texts[key] && !translations[key]) translations[key] = decodeHtml(await translate(texts[key]));
+    if(!texts[key]) {
+      output.value = '실행 중...';
+      texts[key] = await ocrFunc(imgUrlOrB64s[key]) || ' ';
+    }
+    if(texts[key] && !translations[key]) translations[key] = decodeHtml(await transFunc(texts[key]));
+    else output.value = prevOutput;
 
     if(!SINGLE_PAGE_FLAG) output.value = translations.filter(el => el).join('\n----\n');
     else                  output.value = translations[key] || ' ';
 
-    console.log(`page ${key} processed. length: ${translations[key].length}`);
+    console.log(`page ${key} processed. translation length: ${translations[key]?.length}`);
   }
 
   observer.observe(document, {childList: true, subtree: true});
@@ -110,50 +285,24 @@ function decodeHtml(html) {
   return txt.value;
 }
 
-async function ocr(imgUrlOrB64) {
-  //구글 ocr(월 이미지 1000개 무료)
-
-  let reqImage;
-  if(imgUrlOrB64.startsWith('http')) reqImage = {source: {imageUri: imgUrlOrB64}};
-  else reqImage = {content: imgUrlOrB64};
-  
-  let res, data, text;
-  try {
-    res = await fetch(`https://vision.googleapis.com/v1/images:annotate?key=${API_KEY}`, {
-      method: 'POST',
-      body: JSON.stringify({
-        requests: [{
-          features: [{type: "TEXT_DETECTION"}],
-          image: reqImage,
-        }],
-      }),
-    });
-    data = await res.json();
-    text = data.responses?.pop().fullTextAnnotation.text?.replaceAll(/Copyrighted Material/g, '').replaceAll(/\n/gm, '<br/>');
-    //console.log('text:', text);
-  }
-  catch(e) {
-    console.log('google ocr failed', e);
+async function fetchCors(url, payload) {
+  if(payload.body && !payload.data) {
+    //rename .body to .data
+    payload.data = payload.body;
+    delete payload.body;
   }
 
-  return text;
-}
+  //console.debug('payload:', payload);
+  return new Promise((resolve, reject) => {
+    payload.url = url;
+    payload.responseType = 'json';
+    payload.onload = res => {
+      //console.debug('res:', res);
+      resolve(res);
+    };
 
-async function translate(text, target = navigator.language.slice(0, 2)) {
-  //구글 번역(월 50만 자 무료)
-
-  let res, data, translation;
-  try {
-    res = await fetch(`https://translation.googleapis.com/language/translate/v2?key=${API_KEY}&q=${encodeURIComponent(text)}&target=${target}`);
-    data = await res.json();
-    translation = data.data?.translations.pop().translatedText.replaceAll(/<br\/>/g, '\n');
-    //console.log('translation:', translation);
-  }
-  catch(e) {
-    console.log('google trans failed', e);
-  }
-
-  return translation;
+    GM_xmlhttpRequest(payload);
+  });
 }
 
 function sleep(ms) {
@@ -169,7 +318,7 @@ function elementReady_(selector, baseEl = document.documentElement, options = {}
     }
 
     this.prevElNumber = els.length;
-    console.debug('this.prevElNumber, els.length', this.prevElNumber, els.length);
+    console.debug('this.prevElNumber, els.length:', selector, this.prevElNumber, els.length);
 
     new MutationObserver(async (mutationRecords, observer) => {
       let els = [...baseEl.querySelectorAll(selector)];
